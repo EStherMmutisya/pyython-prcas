e9648346d495b853bef27d5a07acadbeaf1bbf8a
EStherMmutisya <starjacob994@gmail.com>
2024-07-17 23:41:21 +0300

    data types

---
+++ ggr.py
@@ -0,0 +1,27 @@
+"""
+name = input('Enter your username').strip().title()
+
+print(f'Hello, {name}!')
+"""
+
+#  types of data types 
+
+   # int (integer)
+   
+   # float (decimal number) - default value  is 1 and  default value is  0. default  value is 
+   
+   
+   
+   
+   # calculatr 
+   
+x = input("what is x?")
+y = input("what is y?")
+
+#  z is the default value- not specified needed for the code generation
+
+# z =int (x)  /int (y)
+
+# print(z)          
+
+print(int (x) / int (y))
