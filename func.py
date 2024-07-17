3512d051666b27f8b18e2791fde9fbe649def6dc
EStherMmutisya <starjacob994@gmail.com>
2024-07-18 00:02:47 +0300

    funcs

---
+++ func.py
@@ -0,0 +1,18 @@
+# def hello( to="world" ):
+#     print("hello," ,to )
+    
+    
+# name = input("what is your name?")
+# # 
+# hello (name)     
+
+
+def main():
+    name = input("what is your name?")
+    hello(name)
+    
+    
+def hello( to="world" ):
+    print("hello," ,to )
+    
+main()
